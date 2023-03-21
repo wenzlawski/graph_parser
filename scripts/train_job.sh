8d75468956288ba53e84a03aaf5396946270c673
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2023-03-21 15:18:47 +0000

    add job

--- scripts/train_job.sh
+++ scripts/train_job.sh
@@ -4,12 +4,15 @@
 #$ -l v100=2
 #$ -pe smp.pe 8
 
+export OMP_NUM_THREADS=$NSLOTS
+
 # load modules
 module load apps/binapps/pytorch/1.11.0-39-gpu-cu113
+module load tools/env/proxy2
 
-export OMP_NUM_THREADS=$NSLOTS
+source activate mlflow
 
 # initialise the environment
 ./examples/multitask_am/install.sh
 ./examples/multitask_am/setup_all.sh
-./examples/multitask_am/mt_all/aaec_essay.sh
+./examples/multitask_am/mt_all/cv.sh
